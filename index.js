ed51c769cd6b00084d25ca8f319467d253d965c2
dinhhungGM <dinhhungGM@gmail.com>
2021-10-09 14:24:20 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+function openSideBar() {
+    document.getElementById("mySidebar").style.display = "block";
+}
+
+function closeSideBar() {
+    document.getElementById("mySidebar").style.display = "none";
+}
